077a7da01d3f67a40f02634500d8ed9a0bc1e9ed
Konrad <15710231+Olimpus444@users.noreply.github.com>
2019-04-25 11:32:18 +0200

    Add Chromedriver

--- TodoAppTests/UnitTest1.cs
+++ TodoAppTests/UnitTest1.cs
@@ -1,14 +1,28 @@
 ﻿using System;
+using System.IO;
+using System.Reflection;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 
 namespace TodoAppTests
 {
+
     [TestClass]
+    [TestCategory("Login")]
     public class UnitTest1
     {
         [TestMethod]
         public void TestMethod1()
         {
+            var driver = GetChromeDriver();
+            driver.Navigate().GoToUrl("slonczynski.github.io");
+        }
+
+        private IWebDriver GetChromeDriver()
+        {
+            var outPutDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            return new ChromeDriver(outPutDirectory);
         }
     }
 }
